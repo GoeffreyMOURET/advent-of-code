f8cc5eede36b8f187ddb498c1830232146e8b991
Geogeos <ggeogeos@gmail.com>
2025-12-22 12:20:41 +0100

    Initial commit

---
+++ src/aoc2025/day6.rs
@@ -0,0 +1,13 @@
+pub struct Day6 {}
+impl Day6 {
+    fn part_one(&mut self) {
+        let stdin = io::stdin();
+        println!("Partie 1 !");
+
+    }
+
+    fn part_two(&mut self) {
+
+    }
+}
+
